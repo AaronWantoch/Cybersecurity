35f710bdd64d8eda8235107426a79728fbf9aae5
AaronWantoch <aaron.wantoch@gmail.com>
2022-05-24 17:17:27 +0200

    Symmetric done

--- Source/display.py
+++ Source/display.py
@@ -1,48 +1,130 @@
 import os
 import time
+
+from Crypto.Cipher import DES3
+from Crypto.Random import get_random_bytes
+
 from utils import Utils
 from asymmetric import Asymmetric
 from symmetric import Symmetric
 import matplotlib.pyplot as plt
+from Crypto.Util import Counter
+from Crypto.Util.number import bytes_to_long
+
 
 class Display:
+    lenghts = [32, 3000, 10000, 50000, 15000000, 30000000, 450000000, 600000000, 900000000]
     # Block must be size of multiple of 2
     @staticmethod
     def drawPlotsAES():
-        #To setup everything
-        randomText = Utils.randomString(10)
-        Display.displayMessageAES(randomText, 256)
-
-        lenghts = [10000, 3000, 50000, 8000, 1000000, 2000000, 3000000]
         encryptionTimes = []
         decryptionTimes = []
-        for i in lenghts:
-            randomText = Utils.randomString(i)
-            encryptionTime, decrytpionTime = Display.displayMessageAES(randomText, 256)
-            encryptionTimes.append(encryptionTime)
-            decryptionTimes.append(decrytpionTime)
-        plt.plot(lenghts, encryptionTimes, marker="o")
-        plt.plot(lenghts, decryptionTimes, marker="o")
-        plt.show()
+        Display.generateDataAES(decryptionTimes, encryptionTimes, Display.lenghts)
+
+        plt.title("AES speed")
+        Display.drawPlots(decryptionTimes, encryptionTimes, Display.lenghts)
 
 
     @staticmethod
-    def displayMessageAES(text, keySize = 256):
-        extended = Utils.extendToMultipleOf2(text) #expands text to multiple of 2
-        bytes = extended.encode("utf-8")
-        key = os.urandom(int(keySize/8))
-        initializationVector = os.urandom(16) #16 bytes
-        baseLength = len(text)
+    def generateDataAES(decryptionTimes, encryptionTimes, lenghts):
+        for i in lenghts:
+            randomText = Display.getPlaintext(i)
+            encryptionTime, decrytpionTime = Display.getTimeAES(randomText, 256)
+            encryptionTimes.append(encryptionTime)
+            decryptionTimes.append(decrytpionTime)
 
+    @staticmethod
+    def getTimeAES(text, keySize = 256):
+        key = get_random_bytes(int(keySize/8))
         start = time.time()
-        encrypted = Symmetric.encryptAES(bytes, key, initializationVector)
+        #print("Text to encrypt:" + text.decode("utf8"))
+        encryptedText, tag, nonce = Symmetric.encryptAES(text, key)
         end = time.time()
         encryptionTime = end-start
-
+        #print("encrypted text: " + encryptedText.decode("utf8"))
         start = time.time()
-        decrypted = Symmetric.decryptAES(encrypted, key, initializationVector)
+        decrypted = Symmetric.decryptAES(encryptedText, key, tag, nonce)
         end = time.time()
         decryptionTime = end-start
+        #print("decrypted text: " + decrypted.decode("utf8"))
+
+        return encryptionTime, decryptionTime
+
+    @staticmethod
+    def drawPlotsSalsa20():
+        encryptionTimes = []
+        decryptionTimes = []
+        Display.generateDataSalsa20(decryptionTimes, encryptionTimes, Display.lenghts)
+
+        plt.title("Salsa20 speed")
+        Display.drawPlots(decryptionTimes, encryptionTimes, Display.lenghts)
+
+
+    @staticmethod
+    def generateDataSalsa20(decryptionTimes, encryptionTimes, lenghts):
+        for i in lenghts:
+            randomText = Display.getPlaintext(i)
+            encryptionTime, decrytpionTime = Display.getTimeSalsa20(randomText, 256)
+            encryptionTimes.append(encryptionTime)
+            decryptionTimes.append(decrytpionTime)
+
+    @staticmethod
+    def getTimeSalsa20(text, keySize = 256):
+        key = get_random_bytes(int(keySize/8))
+        start = time.time()
+        #print("Text to encrypt:" + text.decode("utf8"))
+        encryptedText = Symmetric.encryptSalsa20(text, key)
+        end = time.time()
+        encryptionTime = end-start
+        #print("encrypted text: " + encryptedText.decode("utf8"))
+        start = time.time()
+        decrypted = Symmetric.decryptSalsa20(encryptedText, key)
+        end = time.time()
+        decryptionTime = end-start
+        #print("decrypted text: " + decrypted.decode("utf8"))
+
+        return encryptionTime, decryptionTime
+
+    @staticmethod
+    def drawPlotsDES3():
+        DESlenghts = [32, 15000000, 30000000, 35000000, 40000000, 60000000, 80000000, 90000000, 120000000]
+        encryptionTimes = []
+        decryptionTimes = []
+        Display.generateDataDES3(decryptionTimes, encryptionTimes, DESlenghts)
+
+        plt.title("DES3 speed (smaller data)")
+        Display.drawPlots(decryptionTimes, encryptionTimes, DESlenghts)
+
+
+    @staticmethod
+    def generateDataDES3(decryptionTimes, encryptionTimes, lenghts):
+        for i in lenghts:
+            randomText = Display.getPlaintext(i)
+            encryptionTime, decrytpionTime = Display.getTimeDES3(randomText, 21)
+            encryptionTimes.append(encryptionTime)
+            decryptionTimes.append(decrytpionTime)
+
+    @staticmethod
+    def getTimeDES3(text, keySize = 24):
+        while True:
+            try:
+                key = DES3.adjust_key_parity(get_random_bytes(keySize))
+                break
+            except ValueError:
+                pass
+
+        start = time.time()
+        # print("Text to encrypt:" + str(text))
+        encryptedText, nonce = Symmetric.encryptDES3(text, key)
+        end = time.time()
+        encryptionTime = end-start
+        #print("encrypted text: " + encryptedText.decode("utf8"))
+        start = time.time()
+        decrypted = Symmetric.decryptDES3(encryptedText, key, nonce)
+        end = time.time()
+        decryptionTime = end-start
+        # print("decrypted text: " + str(decrypted))
+
 
         return encryptionTime, decryptionTime
 
@@ -61,4 +143,20 @@ class Display:
         print("AES decrypted message. Decrypted message is: ")
         print(decrypted)
         print("It took ", end - start, " s.")
+
+
+
+    # Gets bytes of text
+    @staticmethod
+    def getPlaintext(i):
+        return get_random_bytes(i)
+
+    @staticmethod
+    def drawPlots(decryptionTimes, encryptionTimes, lenghts):
+        plt.plot(lenghts, encryptionTimes, marker="o", label="encryption time")
+        plt.plot(lenghts, decryptionTimes, marker="o", label="decryption time")
+        plt.legend()
+        plt.xlabel("Length of text")
+        plt.ylabel("time [s]")
+        plt.show()
 
--- Source/main.py
+++ Source/main.py
@@ -2,5 +2,7 @@ from display import Display
 from utils import Utils
 
 if __name__ == '__main__':
+    Display.drawPlotsDES3()
+    Display.drawPlotsSalsa20()
     Display.drawPlotsAES()
 
