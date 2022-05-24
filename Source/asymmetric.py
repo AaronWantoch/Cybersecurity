5a2f1bd9a76fff1176b4c57b652b2ca3c4c4ea99
Zofia Sosińska <z.sosinska001@gmail.com>
2022-05-24 13:33:44 +0200

    ECC v1 works

--- Source/asymmetric.py
+++ Source/asymmetric.py
@@ -1,11 +1,12 @@
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import utils, padding, rsa
-from cryptography.hazmat.primitives.ciphers.algorithms import AES
+from Crypto.Cipher import AES
 from tinyec import registry
 import hashlib, secrets, binascii
 
 
 class Asymmetric:
+    #A brainpoolP256r1 named curve
     curve = registry.get_curve('brainpoolP256r1')
     """
     -RSA keys have a complex internal structure with specific mathematical properties.
@@ -61,7 +62,7 @@ class Asymmetric:
     def encrypt_AES_GCM(self, msg, secretKey):
         aesCipher = AES.new(secretKey, AES.MODE_GCM)
         ciphertext, authTag = aesCipher.encrypt_and_digest(msg)
-        return (ciphertext, aesCipher.nonce, authTag)
+        return ciphertext, aesCipher.nonce, authTag
 
     def decrypt_AES_GCM(self, ciphertext, nonce, authTag, secretKey):
         aesCipher = AES.new(secretKey, AES.MODE_GCM, nonce)
@@ -81,7 +82,7 @@ class Asymmetric:
         secretKey = self.ecc_point_to_256_bit_key(sharedECCKey)
         ciphertext, nonce, authTag = self.encrypt_AES_GCM(msg, secretKey)
         ciphertextPubKey = ciphertextPrivKey * self.curve.g
-        return (ciphertext, nonce, authTag, ciphertextPubKey)
+        return ciphertext, nonce, authTag, ciphertextPubKey
 
     def decrypt_ECC(self, encryptedMsg, privKey):
         (ciphertext, nonce, authTag, ciphertextPubKey) = encryptedMsg
@@ -92,64 +93,22 @@ class Asymmetric:
 
 
 # =============================================================
+
+asym = Asymmetric()
 msg = b'Text to be encrypted by ECC public key and ' \
       b'decrypted by its corresponding ECC private key'
 print("original msg:", msg)
-'''
-The above example starts from generating an ECC public + private key pair 
-for the message recipient: pubKey + privKey, using the tinyec library. These
-keys will be used to encrypt the message msg through the hybrid encryption 
-scheme (asymmetric ECC + symmetric AES) and to decrypt is later back to its original form.
-''
-privKey = secrets.randbelow(curve.field.n)
-pubKey = privKey * curve.g
-''
-Next, we encrypt msg by using the pubKey and we obtain as a result the following set of output:
- { ciphertext, nonce, authTag, ciphertextPubKey }
-''
-encryptedMsg = encrypt_ECC(msg, pubKey)
+privKey = secrets.randbelow(asym.curve.field.n)
+pubKey = privKey * asym.curve.g
+encryptedMsg = asym.encrypt_ECC(msg, pubKey)
 encryptedMsgObj = {
     'ciphertext': binascii.hexlify(encryptedMsg[0]),    # obtained by the symmetric AES-GCM encryption
     'nonce': binascii.hexlify(encryptedMsg[1]),         # random AES initialization vector
     'authTag': binascii.hexlify(encryptedMsg[2]),       # the MAC code of the encrypted text, obtained by the GCM block mode
-    ''
-    Additionally, we obtain a randomly generated ephemeral public key ciphertextPubKey,
-    which will be encapsulated in the encrypted message and will be used to recover the
-    AES symmetric key during the decryption (using the ECDH key agreement scheme)
-    ''
     'ciphertextPubKey': hex(encryptedMsg[3].x) + hex(encryptedMsg[3].y % 2)[2:]
 }
 print("encrypted msg:", encryptedMsgObj)
-''
-To decrypt the encrypted message, we use the data produced during the encryption
-along with the decryption privateKey
-''
-decryptedMsg = decrypt_ECC(encryptedMsg, privKey)
+
+decryptedMsg = asym.decrypt_ECC(encryptedMsg, privKey)
 print("decrypted msg:", decryptedMsg)
 
-# =============================================================
-
-"""
-
-is it needed?
-
-        signature = private_key.sign(
-            messageByteFormat,
-            padding.PSS(
-                mgf=padding.MGF1(hashes.SHA256()),
-                salt_length=padding.PSS.MAX_LENGTH
-            ),
-            hashes.SHA256()
-        )
-
-        # verification
-        public_key.verify(
-            signature,
-            messageByteFormat,
-            padding.PSS(
-                mgf=padding.MGF1(hashes.SHA256()),
-                salt_length=padding.PSS.MAX_LENGTH
-            ),
-            hashes.SHA256()
-        )
-"""'''
